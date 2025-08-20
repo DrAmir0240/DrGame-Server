6afbaaafe3754735bbe6b9921484f50c9833c9a0
DrAmir0240 <amirhosseinomidi1383@gmail.com>
2025-08-20 12:58:44 +0330

    fixing errors and bugs on customer 3

--- customers/urls.py
+++ customers/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('profile/complete/', views.CustomerProfileCreateAPIView.as_view(), name='create-customer-profile'),
     path('profile/', views.CustomerProfileRetrieveAPIView.as_view(), name='profile'),
-    path('profile/upgrade/', views.UpgradeToBusinessCustomerCreateAPIView.as_view(), name='upgrade-to-business'),
 
     # order list & retrieve urls
     path('orders/', views.CustomerOrderListAPIView.as_view(), name='customer-orders'),
